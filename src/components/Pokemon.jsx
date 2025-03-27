57e5e5ed632e6d12dc8395615ce66fa7c7b27867
Kasador <hunterstevenshaw@gmail.com>
2025-03-26 22:17:30 -0700

    ⇄ API calls are pulling data dynamically.

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,19 +1,63 @@
 import { useState, useEffect } from 'react';
 import PokemonService from '../services/PokemonService';
 import { FaArrowAltCircleUp, FaArrowCircleDown  } from "react-icons/fa"; // import arrows from react-icons - npm install react-icons
+import axios from 'axios';
 
 const Pokemon = () => {
     const [pokemonData, setPokemonData] = useState(null); // setup the state to save the data
+    const [pokemonInfo, setPokemonInfo] = useState(null);
+    const [loadedPokemon, setLoadedPokemon] = useState(null);
 
     useEffect(() => {
         const fetchPokemonData = async () => {
-            const data = await PokemonService();
-            setPokemonData(data); // save the data in the state
-
-        }
+            try {
+                const fetchPokemon = await PokemonService();
+                setPokemonData(fetchPokemon);
+                setLoadedPokemon(true);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
 
         fetchPokemonData();
     }, []);
+
+    const getPokemonDataByName = async(name) => {
+        setLoadedPokemon(false); // load a different pokemon
+        console.log('You clicked: ', name);
+
+        let pokemonName = name;
+        // let pokemonNamesDataArray = pokemonData.pokemons.results;
+        const getPokiByName = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const getPokemonInfo = getPokiByName.data;
+
+        console.log('..and here is the data: ', getPokemonInfo);
+
+        setPokemonInfo(getPokemonInfo);
+
+        // const findPokemonInfo = pokemonNamesDataArray.find(pokemon => pokemon.name === pokemonName);
+
+        // console.log(findPokemonInfo);
+        // setPokemonInfo(findPokemonInfo);
+
+        // const fetchPokemonInfo = fetchPokemonByName(findPokemonInfo.name);
+        // console.log('You clicked: ', fetchPokemonInfo);
+    }
+
+    // const fetchPokemonByName = (name) => {
+    //     const resTwo = axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    //     const pokemonInfo = resTwo.data;
+
+    //     console.log('clicked named: ', pokemonInfo);
+
+    // //    const pokemonData = {
+    // //         "pokemons": fetchPokemon,
+    // //         "pokemon": pokemonInfo
+    // //    };
+
+    //    console.log(pokemonData);
+    //    return pokemonInfo;
+    // }
 
     return (
         <>
@@ -23,18 +67,50 @@ const Pokemon = () => {
             <span className='pokemon-scroll-title'>Use the mouse wheel to scroll </span>
             <div className='pokemon-search-container'>
                 {pokemonData && ( // this checks to if if the data is fetched before rendering the components
+                <div>
                     <ol>
-                        {pokemonData.results.map((pokemon) => (
+                        {pokemonData.pokemons.results.map((pokemon) => (
                             <button key={pokemon.name}>
                                 <li 
                                     key={pokemon.name} 
                                     value={pokemon.name} 
-                                    onClick={() => {console.log(pokemon.name)}}>{pokemon.name}</li>
+                                    onClick={() => {
+                                        getPokemonDataByName(pokemon.name);
+                                    }}>{pokemon.name}</li>
                             </button>
                         ))}
                     </ol>
+                </div>
                 )}
-            </div>
+            </div>|
+            {pokemonInfo && (
+            <div className='pokemon-info-container'>
+                <section>
+                    <img src={pokemonData.pokemon.sprites.front_default} alt={pokemonData.pokemon.name} />
+                    <h1>{pokemonData.pokemon.name}</h1>
+                </section>
+                <h2>Height: {pokemonInfo.height}</h2>
+                <h2>Weight: {pokemonInfo.weight}</h2>
+                <h2>Base Experience: {pokemonInfo.base_experience}</h2>
+                <h2>Abilities: {pokemonInfo.abilities.map((ability) => ability.ability.name).join(', ')}</h2>
+                <h2>Types: {pokemonInfo.types.map((type) => type.type.name).join(', ')}</h2>
+                <h2>Stats: {pokemonInfo.stats.map((stat) => stat.stat.name).join(', ')}</h2>
+            </div>)}
+            {loadedPokemon && (
+            <div className='pokemon-info-container'>
+                <section>
+                    <img src={pokemonData.pokemon.sprites.front_default} alt={pokemonData.pokemon.name} />
+                    <h1>{pokemonData.pokemon.name}</h1>
+                </section>
+                <article>
+                    <h2>Height: {pokemonData.pokemon.height}</h2>
+                    <h2>Weight: {pokemonData.pokemon.weight}</h2>
+                    <h2>Base Experience: {pokemonData.pokemon.base_experience}</h2>
+                    <h2>Abilities: {pokemonData.pokemon.abilities.map((ability) => ability.ability.name).join(', ')}</h2>
+                    <h2>Types: {pokemonData.pokemon.types.map((type) => type.type.name).join(', ')}</h2>
+                    <h2>Stats: {pokemonData.pokemon.stats.map((stat) => stat.stat.name).join(', ')}</h2>
+                </article>
+            </div>)}
         </>
     )
 }
--- src/services/PokemonService.jsx
+++ src/services/PokemonService.jsx
@@ -2,17 +2,44 @@ import axios from 'axios'; // use axios to handle the fetch request
 
 const PokemonService = async () => { // func to fetch the pokemon data
     try { // try to fetch the data
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000'); // the API endpoint
-        const data = response.data; // the res from the API
+        const resOne = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000') // the API endpoint
+        const allPokemon = resOne.data; // the res from the API
 
-        // add error handling
+        console.log(allPokemon);
 
-        console.log(data); // log out the data
-        return data; // return the data in this func, so when called I can have access to it
+        // get random number
+        const randomNumber = Math.floor(Math.random() * allPokemon.results.length);
+        let name = allPokemon.results[randomNumber].name;
+
+        const resTwo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pokemonInfo = resTwo.data;
+
+        console.log('First Loaded Pokemon: ', pokemonInfo);
+
+       const pokemonData = {
+            "pokemons": allPokemon,
+            "pokemon": pokemonInfo
+       };
+
+       console.log(pokemonData);
+
+       return pokemonData;
     } catch (error) { // if there was an error, then we will log it out and throw error 
         console.error('Error fetching the Pokemon data requested:', error);
         throw error;
     }
 }
+
+// const getPokemonDataByName = async (name) => {
+//     try {
+//         let name = props.name;
+//         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+//         const data = response.data;
+//         return data;
+//     } catch (error) {
+//         console.error('Error fetching the Pokemon data requested:', error);
+//         throw error;
+//     }
+// }
 
 export default PokemonService;
